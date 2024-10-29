3fdacda47c5af1cdef9952cb192197221042aa9b
nandaMaria08 <maria.costa1394@aluno.ce.gov.br>
2024-10-29 16:47:09 -0300

    pequenas alterações

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,15 +5,17 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                     <a href="{{ route('dashboard') }}">
+                        <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" class="h-12 w-auto" /> 
                     </a>
                 </div>
+
+
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }} 
+                        {{ __('Controle de Estoque') }} 
                     </x-nav-link>
                 </div>
             </div>
