136ced2a6c571fdcf2051dd73f0fc5fac1912df9
nandaMaria08 <maria.costa1394@aluno.ce.gov.br>
2024-11-06 14:54:17 -0300

    mudanças na dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,9 +11,37 @@
         </div>
     </div>  -->
 
-    <div class="px-6 pt-10">
+    <div class="px-6 pt-10 flex flex-wrap justify-center">
         <div class="max-w-xs rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="{{asset('assets/img/marcas.jpg')}}" alt="Sunset in the mountains">
+            <img class="w-full" src="{{asset('assets/img/marcas.jpg')}}" alt="marcas">
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Cadastrar Marcas</div>
+                    <p class="text-gray-700 text-base">
+                         Cadastre aqui as marcas existentes na sua loja!
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                <button class="bg-red-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded">
+                     Cadastrar
+                </button>
+                </div>
+            </div>
+        </div>
+        <div class="max-w-xs rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="{{asset('assets/img/marcas.jpg')}}" alt="produtos">
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Cadastrar Marcas</div>
+                    <p class="text-gray-700 text-base">
+                         Cadastre aqui as produtos disponíveis na sua loja!
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                <button class="bg-red-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded">
+                     Cadastrar
+                </button>
+                </div>
+            </div>
+        </div>
+        <div class="max-w-xs rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="{{asset('assets/img/marcas.jpg')}}" alt="ciclos">
             <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">Cadastrar Marcas</div>
                     <p class="text-gray-700 text-base">
