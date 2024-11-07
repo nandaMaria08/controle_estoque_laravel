16e8c8f7bffcbb520277acc9d06c0cfbe2d0fb0d
nandaMaria08 <maria.costa1394@aluno.ce.gov.br>
2024-11-07 14:40:55 -0300

    Identação

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,51 +12,51 @@
     </div>  -->
 
     <div class="px-20 pt-10 flex flex-wrap justify-between gap-6">
-    <div class="max-w-xs w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-lg">
-        <img class="w-full" src="{{asset('assets/img/marcas.jpg')}}" alt="marcas">
-        <div class="px-6 py-2">
-            <div class="font-bold text-xl mb-2">Cadastrar Marcas</div>
-            <p class="text-gray-700 text-base">
-                Cadastre aqui as marcas existentes na sua loja!
-            </p> 
+        <div class="max-w-xs w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="{{asset('assets/img/marcas.jpg')}}" alt="marcas">
+            <div class="px-6 py-2">
+                <div class="font-bold text-xl mb-2">Cadastrar Marcas</div>
+                <p class="text-gray-700 text-base">
+                    Cadastre aqui as marcas existentes na sua loja!
+                </p> 
+            </div>
+            <div class="px-6 pt-4 pb-2">
+                <button class="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
+                    Cadastrar
+                </button>
+            </div>
         </div>
-        <div class="px-6 pt-4 pb-2">
-            <button class="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
-                Cadastrar
-            </button>
-        </div>
-    </div>
 
-    <div class="max-w-xs w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-lg">
-        <img class="w-full" src="{{asset('assets/img/cosmeticos.jpg')}}" alt="cosmeticos">
-        <div class="px-6 py-2">
-            <div class="font-bold text-xl mb-2">Cadastrar Produtos</div>
-            <p class="text-gray-700 text-base">
-                Cadastre aqui os produtos disponíveis na sua loja!
-            </p> 
+        <div class="max-w-xs w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="{{asset('assets/img/cosmeticos.jpg')}}" alt="cosmeticos">
+            <div class="px-6 py-2">
+                <div class="font-bold text-xl mb-2">Cadastrar Produtos</div>
+                <p class="text-gray-700 text-base">
+                    Cadastre aqui os produtos disponíveis na sua loja!
+                </p> 
+            </div>
+            <div class="px-6 pt-4 pb-2">
+                <button class="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
+                    Cadastrar
+                </button>
+            </div>
         </div>
-        <div class="px-6 pt-4 pb-2">
-            <button class="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
-                Cadastrar
-            </button>
-        </div>
-    </div>
 
-    <div class="max-w-xs w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-lg">
-        <img class="w-full" src="{{asset('assets/img/relatorio.jpg')}}" alt="pedidos">
-        <div class="px-6 py-2">
-            <div class="font-bold text-xl mb-2">Cadastrar Pedidos</div>
-            <p class="text-gray-700 text-base">
-                Cadastre aqui as datas de chegada dos seus pedidos e sua marca!
-            </p> 
-        </div>
-        <div class="px-6 pt-4 pb-2">
-            <button class="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
-                Cadastrar
-            </button>
+        <div class="max-w-xs w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="{{asset('assets/img/relatorio.jpg')}}" alt="pedidos">
+            <div class="px-6 py-2">
+                <div class="font-bold text-xl mb-2">Cadastrar Pedidos</div>
+                <p class="text-gray-700 text-base">
+                    Cadastre aqui as datas de chegada dos seus pedidos e sua marca!
+                </p> 
+            </div>
+            <div class="px-6 pt-4 pb-2">
+                <button class="bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
+                    Cadastrar
+                </button>
+            </div>
         </div>
     </div>
-</div>
 
 
 </x-app-layout>
