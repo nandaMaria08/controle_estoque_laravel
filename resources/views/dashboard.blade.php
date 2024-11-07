fa252b15bf03d7e78cac051c7c4f02c6071e9189
nandaMaria08 <maria.costa1394@aluno.ce.gov.br>
2024-11-07 14:30:01 -0300

    responsividade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,10 +11,10 @@
         </div>
     </div>  -->
 
-    <div class="px-20 pt-10 flex flex-wrap justify-between">
-    <div class="max-w-xs rounded overflow-hidden shadow-lg ">
+    <div class="px-20 pt-10 flex flex-wrap justify-between gap-6">
+    <div class="max-w-xs w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-lg">
         <img class="w-full" src="{{asset('assets/img/marcas.jpg')}}" alt="marcas">
-        <div class="px-6 py-4">
+        <div class="px-6 py-2">
             <div class="font-bold text-xl mb-2">Cadastrar Marcas</div>
             <p class="text-gray-700 text-base">
                 Cadastre aqui as marcas existentes na sua loja!
@@ -27,9 +27,9 @@
         </div>
     </div>
 
-    <div class="max-w-xs rounded overflow-hidden shadow-lg ">
-        <img class="w-full" src="{{asset('assets/img/marcas.jpg')}}" alt="produtos">
-        <div class="px-6 py-4">
+    <div class="max-w-xs w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="{{asset('assets/img/cosmeticos.jpg')}}" alt="cosmeticos">
+        <div class="px-6 py-2">
             <div class="font-bold text-xl mb-2">Cadastrar Produtos</div>
             <p class="text-gray-700 text-base">
                 Cadastre aqui os produtos disponíveis na sua loja!
@@ -42,12 +42,12 @@
         </div>
     </div>
 
-    <div class="max-w-xs rounded overflow-hidden shadow-lg ">
-        <img class="w-full" src="{{asset('assets/img/marcas.jpg')}}" alt="ciclos">
-        <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Cadastrar Ciclos</div>
+    <div class="max-w-xs w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="{{asset('assets/img/relatorio.jpg')}}" alt="pedidos">
+        <div class="px-6 py-2">
+            <div class="font-bold text-xl mb-2">Cadastrar Pedidos</div>
             <p class="text-gray-700 text-base">
-                Cadastre aqui os ciclos de vendas da sua loja!
+                Cadastre aqui as datas de chegada dos seus pedidos e sua marca!
             </p> 
         </div>
         <div class="px-6 pt-4 pb-2">
@@ -57,5 +57,6 @@
         </div>
     </div>
 </div>
+
 
 </x-app-layout>
