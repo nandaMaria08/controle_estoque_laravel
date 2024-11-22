4eabec14e865965ddc3c2441da0066e18414a69e
nandaMaria08 <maria.costa1394@aluno.ce.gov.br>
2024-11-22 19:56:05 -0300

    identação

--- resources/views/mark_create.blade.php
+++ resources/views/mark_create.blade.php
@@ -49,23 +49,27 @@
                 <div class="w-full max-w-4xl bg-white p-8 rounded-lg shadow-lg">
                     <h1 class="text-center text-2xl font-semibold mb-6">Cadastre aqui as marcas disponíveis na sua loja!</h1>
                     <hr class="mb-6">
-                    <form class="grid gap-6 mb-6 grid-cols-1 md:grid-cols-2" action="{{route('marks.store')}}" method="POST">
+                    <form class="grid gap-6 mb-6 grid-cols-1 md:grid-cols-2" action="{{ route('marks.store') }}" method="POST">
                         @csrf 
 
+            
                         <div class="flex items-center space-x-4">
                             <div class="flex-1">
-                            <label class="mb-2 text-sm font-medium text-gray-900 dark:text-white" for="marca">Marca</label>
-                            <input type="text" id="mark" name="mark" class="min-w-[300px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-200 focus:border-red-200 block p-2.5 dark:bg-red-700 dark:border-red-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 w-full" placeholder="Escreva a marca" required>        
+                                <label class="mb-2 text-sm font-medium text-gray-900 dark:text-white" for="mark">Marca</label>
+                                <input type="text" id="mark" name="mark" class="min-w-[300px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-200 focus:border-red-200 block p-2.5 dark:bg-red-700 dark:border-red-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 w-full" placeholder="Escreva a marca" required>
+                            </div>
                         </div>
-                        <div class="pt-7">
+
+           
+                        <div class="pt-7 flex justify-center md:justify-start">
                             <button type="submit" class="bg-red-300 hover:bg-red-400 text-white font-semibold py-2 px-4 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-400 text-sm w-full md:w-auto">
                                 Cadastrar
                             </button>
                         </div>
-                        </div>
-                     </form>
+                    </form>
                 </div>
             </main>
+
         </div>
         
     </body>
