43401db896e05f07ad66b96ca2d8527e7072060b
Pereirapam <pereirapamela690@gmail.com>
2025-04-05 23:00:50 -0300

    migration

--- database/migrations/2025_04_06_012124_create_requests_table.php
+++ database/migrations/2025_04_06_012124_create_requests_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('requests', function (Blueprint $table) {
             $table->id();
+            $table->string('tipo');
+            $table->date('data_chegada');
+            $table->string('ciclo');
+            $table->foreignId('marca_id')->constrained('marks')->onDelete('cascade');
             $table->timestamps();
         });
     }
