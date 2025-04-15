837acc132a7f60aa8a35323b318ee1daa8422a6b
nandaMaria08 <mariafernandasombracosta@gmail.com>
2025-04-15 20:15:49 -0300

    mudanças na dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -78,7 +78,7 @@
 
 
             <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 max-w-xs rounded overflow-hidden shadow-lg md:ml-6">
-                <img class="w-full h-48 object-cover" src="{{asset('assets/img/pedidos.jpeg')}}" alt="clientes">
+                <img class="w-full h-48 object-cover" src="{{asset('assets/img/clientes.png')}}" alt="clientes">
                 <div class="px-6 py-2">
                     <div class="font-bold text-xl mb-2">Cadastrar Clientes</div>
                     <p class="text-gray-700 text-base">
